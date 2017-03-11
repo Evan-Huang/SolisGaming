f039c8b74a3c9c1e7a92842a0424a3354aa6565d
JaCa <2628685929@qq.com>
2017-03-11 20:32:40 +0800

    add benefits.php

---
+++ en/benefits.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<title>Home | Solis</title>
+
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="./css/jquery.vm-carousel.css">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<link href="style/main.css" rel="stylesheet">
+<link href="style/benefit.css" rel="stylesheet">
+<script src="js/modernizr.js"></script>
+</head>
+<body class="benefits">
+<!-- Header -->
+<?php
+include_once 'header.html';
+?>
+<!-- /Header -->
+<div class="container-fluid" style="z-index: 1">
+  <div class="container ">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="hero_banner benefit" style="background-image: url(images/promotion_banner.jpg)">
+        <div class="benefit_tagline">
+        	<h2>BENEFITS GRID</h2>
+        	<p>Discover the benefits of being a Club Solis member.</p>
+          </div>
+       </div>
+      </div>
+    </div>
+    <div class="row bg_image">
+        <div class="benefit_box">
+          <div class="col-lg-12">
+            <div class="crumb">
+              <a href="index.php">Home</a> >
+              <a href="#">Benefits</a>
+            </div>
+          </div>
+
+            <div class="col-lg-12">
+                <div class="benefit_content">
+                    <p class="welcome">Welcome to the world of Club SOLIS, one of the most preferred membership program offers the exciting experience, the endless rewards, exclusive privileges and personalized customer service.</p>
+                    <p class="action">Action now to enter the globe where only the best experiences and wide array of amazing rewards await you!</p>
+
+                    <h4 class="membership">CLUB SOLIS MEMBERSHIP</h4>
+                    <div class="row">
+                        <div class="col-md-6 membership_left"><p>In Club SOLIS, its 4 tiers of prestige memberships; EQUINOX, SOLISTICE, CELESTIAL and STAR, offers an extensive array of world class privileges and exciting offers ranging from hotel accommodation & spa, fine dining, complimentary transportation, the latest electronic gadgets, luxury shopping voucher and much much more...</p></div>
+                        <div class="col-md-6 membership_right"><p>With a linked membership account, our prestige guests can earn the rewarding points in any of our slot lounges. Club Solis is focused on delivering convenience and accelerated rewards points to redeem the precious gifts you especially desire.</p></div>
+                    </div>
+
+                    <h4 class="points">HOW TO EARN CLUB SOLIS POINTS</h4>
+                    <div class="table-responsive">
+                        <table class="table points_table">
+                            <tbody>
+                            <tr>
+                                <td><p>Slot Coin In $40</p></td>
+                                <td><p>= 1 point</p></td>
+                                <td><p>10 points = $1</p></td>
+                            </tr>
+                            <tr>
+                                <td><p>Electronic Roulette and Baccarat Coin In $200</p></td>
+                                <td><p>= 1 point</p></td>
+                                <td><p>10 points = $1</p></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+
+                    <h4 class="rewardsandoffers">REWARDS AND OFFERS</h4>
+                    <div class="row cards" >
+                        <div class="col-md-2 col-md-offset-3"><img id="card1" src="images/card_face_4.png" alt="" class="img-responsive center-block" style=""><p>EQUINOX</p></div>
+                        <div class="col-md-2" ><img id="card2" src="images/card_face_3.png" alt="" class="img-responsive center-block" style=""><p>SOLSTICE</p></div>
+                        <div class="col-md-2" ><img id="card3" src="images/card_face_2.png" alt="" class="img-responsive center-block" style=""><p>CELESTIAL</p></div>
+                        <div class="col-md-2" ><img id="card4" src="images/card_face_1.png" alt="" class="img-responsive center-block" style=""><p>STAR</p></div>
+                    </div>
+                    <div class="row cards_detail">
+                        <div class="col-md-2 col-md-offset-3"><p>Exlusively avaialble to invited guests only. Prestige Personalized Customer Sevice and facilities offered.</p></div>
+                        <div class="col-md-2"><p>Earn 26,888 points or more in the last 6 months.</p></div>
+                        <div class="col-md-2"><p>Earn 3,888 points or more in the last 6 months.</p></div>
+                        <div class="col-md-2"><p>Welcome to the world of Club SOLIS and open the door to victory and exciting privileges!</p></div>
+                    </div>
+
+                    <div class="row discount_redemption">
+                        <div class="col-lg-12">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <caption class="caption">Discount</caption>
+                                    <tbody>
+                                    <tr>
+                                        <td>Restaurant Grandview</td>
+                                        <td>20%</td>
+                                        <td>15%</td>
+                                        <td>15%</td>
+                                        <td>10%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Restaurant Case Real</td>
+                                        <td>15%</td>
+                                        <td>10%</td>
+                                        <td>10%</td>
+                                        <td>5%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Restaurant(Landmark-Varanda,King of kings,Petrus)</td>
+                                        <td>15%</td>
+                                        <td>15%</td>
+                                        <td>10%</td>
+                                        <td>10%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Spa(Landmark)</td>
+                                        <td>15%</td>
+                                        <td>15%</td>
+                                        <td>10%</td>
+                                        <td>10%</td>
+                                    </tr>
+
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+
+                        <div class="col-lg-12">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <caption class="caption">Redemption(Not application to discounts)</caption>
+                                    <tbody>
+                                    <tr>
+                                        <td>Dinging,spa,Accommodation</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Valet Parking</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Solis Retail Redemption Program</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Ferry Tickets</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                        <td>√</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Limousine</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td>√</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Airfares</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td>√</td>
+                                    </tr>
+
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    </div>
+                </div>
+        </div>
+    </div>
+    </div>
+  </div>
+
+<footer>
+    <!-- Footer -->
+    <?php
+    include_once 'footer.html';
+    ?>
+    <!-- /Footer -->
+</footer>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.vm-carousel.js"></script>
+<script src="js/index.js"></script>
+<script>
+
+    $(window).scroll(function (event) {
+
+
+        if ($(this).scrollTop() > 50) {
+
+            $("#wrap").addClass("fix-menu");
+
+        } else {
+
+            $("#wrap").removeClass("fix-menu");
+
+        }
+
+    });
+
+    $(document).ready(function(){
+
+        $('.li_bt_home').removeClass('active');
+        $("#bs-navbar-collapse-1 li[class='dropdown']").addClass('active');
+
+    });
+
+    //    返回上一頁面
+    window.document.onkeydown = disableRefresh;
+    function disableRefresh(evt){
+        evt = (evt) ? evt : window.event
+        if (evt.keyCode == 8) {
+            history.back();
+        }
+    }
+
+
+
+	</script>
+</body>
+</html>
