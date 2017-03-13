77d0645feee8677cff616d2630f464e7eafbb592
JaCa <2628685929@qq.com>
2017-03-13 15:58:34 +0800

    add redemption-program.php

---
+++ en/redemption-program.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Home | Solis</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="./css/jquery.vm-carousel.css">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<link href="style/main.css" rel="stylesheet">
+	<link href="style/redemptionprogram.css" rel="stylesheet">
+	<script src="js/modernizr.js"></script>
+</head>
+
+<body>
+	<!-- Header -->
+	<?php
+	include_once 'header.html';
+	?>
+	<!-- /Header -->
+	<div class="container-fluid" style="z-index: 1">
+		<div class="container ">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="hero_banner redemption_program" style="background-image: url(images/promotion_banner_none.jpg)">
+						<div class="redemption_program_tagline">
+							<h2>Redemption Program</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row bg_image">
+				<div class="redemption_program_box">
+					<div class="col-lg-12 hidden-xs">
+						<div class="crumb">
+							<a href="index.php">Home</a> >
+							<a href="#">Redemption Program</a>
+						</div>
+					</div>
+
+					<div class="col-lg-12">
+						<div class="redemption_program_content">
+							<p class="staytuned">Stay tuned for the exciting promotions exclusively for our prestige Club SOLIS members</p>
+
+							<h4 class="redemptionitem">REDEMPTION ITEM</h4>
+
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="item_box">
+                                        <img style="height: 340px;" src="./images/card_face_1.png" class="img-responsive">
+                                        <div class="item_content">
+                                            <p>iPhone 7 plus 128GB</p>
+                                            <p>Points</p><p class="pull-right">85,000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="item_box">
+                                        <img style="height: 340px;" src="./images/card_face_1.png" class="img-responsive">
+                                        <div class="item_content">
+                                            <p>iPhone 7 plus 128GB</p>
+                                            <p>Points</p><p class="pull-right">85,000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="item_box">
+                                        <img style="height: 340px;" src="./images/card_face_1.png" class="img-responsive">
+                                        <div class="item_content">
+                                            <p>iPhone 7 plus 128GB</p>
+                                            <p>Points</p><p class="pull-right">85,000</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="item_box">
+                                        <img style="height: 340px;" src="./images/card_face_1.png" class="img-responsive">
+                                        <div class="item_content">
+                                            <p>iPhone 7 plus 128GB</p>
+                                            <p>Points</p><p class="pull-right">85,000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="item_box">
+                                        <img style="height: 340px;" src="./images/card_face_1.png" class="img-responsive">
+                                        <div class="item_content">
+                                            <p>iPhone 7 plus 128GB</p>
+                                            <p>Points</p><p class="pull-right">85,000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="item_box">
+                                        <img style="height: 340px;" src="./images/card_face_1.png" class="img-responsive">
+                                        <div class="item_content">
+                                            <p>iPhone 7 plus 128GB</p>
+                                            <p>Points</p><p class="pull-right">85,000</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="item_box">
+                                        <img style="height: 340px;" src="./images/card_face_1.png" class="img-responsive">
+                                        <div class="item_content">
+                                            <p>iPhone 7 plus 128GB</p>
+                                            <p>Points</p><p class="pull-right">85,000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="item_box">
+                                        <img style="height: 340px;" src="./images/card_face_1.png" class="img-responsive">
+                                        <div class="item_content">
+                                            <p>iPhone 7 plus 128GB</p>
+                                            <p>Points</p><p class="pull-right">85,000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="item_box">
+                                        <img style="height: 340px;" src="./images/card_face_1.png" class="img-responsive">
+                                        <div class="item_content">
+                                            <p>iPhone 7 plus 128GB</p>
+                                            <p>Points</p><p class="pull-right">85,000</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <h4 class="terms">TERMS AND DONDITIONS</h4>
+
+                            <div class="terms_list">
+                                <ol>
+                                    <li>Club SOLIS members is only open to players aged 21 years or above.</li>
+                                    <li>Players can apply to our Club SOLIS members at SOLIS Grandview 1st Floor, SOLIS Casa Real 1st & 2nd Floor, and SOLIS Landmark 3rd Floor, on each venue, each Club SOLIS member will be issued with one membership card with one membership number.</li>
+                                    <li>No membership fee is required.</li>
+                                    <li>All Club SOLIS members’ belonging, like membership, account, points and discount are not transferable.</li>
+                                    <li>Club SOLIS members are required to present their Club SOLIS membership cards and identity (Such as passport, travel documents, ID card or residence card) upon any service counter, restaurant and business partner while doing some authentication; such as reprint membership card, points redemption, prize awards, using shopping vouchers, participate promotions and events, apply special discount, etc. </li>
+                                    <li>Club SOLIS members can earn points at SOLIS Casa Real, SOLIS Grandview and SOLIS Landmark.  Members can have point redemption at any SOLIS venues.</li>
+                                    <li>When there is not gaming record within the last 12 months, all rewards will be forfeited.</li>
+                                    <li>Solstice and Celestial membership are valid for one year.  Every end of the year, the membership holders will be assessed.  If not reach that membership requirement, will be offer another level of membership to the players.</li>
+                                    <li>Club SOLIS reserves the rights to revise the point earning record and should have the final decision on all the judgments.</li>
+                                    <li>Hotel, Restaurant, Spa discount or benefits are only suitable for specific items, not suitable for events or private party, and bill settlement only accept cash or credit card.</li>
+                                    <li>Points will be deducted from account automatically when redemptions are claimed.</li>
+                                    <li>Points and redemption items cannot be redeemed for cash. No further exchange  after the redemption has been made.</li>
+                                    <li>Club SOLIS members cannot share membership cards to earn point</li>
+                                    <li>Improper use of membership benefits, awards, point’s redemption or discount offers, membership benefits will be forfeited.</li>
+                                    <li>Club SOLIS is not responsible for any lost or stolen card.</li>
+                                    <li>Club SOLIS member’s use of the membership card indicates acceptance of conditions as listed.</li>
+                                    <li>Club SOLIS reserves the right to forfeited or cancel any membership or members benefits or reject the some players be our member, without prior notice or compensation.</li>
+                                    <li>Become Club SOLIS members represent willing to receive SOLIS, hotel and business parties’ information and news.</li>
+                                    <li>All promotion and event may conduct any form of photographic or/and video recording. SOLIS reserves the right to broadcast all images at its discretion.</li>
+                                    <li>Employees and/or relatives of SOLIS, parent companies and advertising agencies or representatives thereof, are not eligible to participate in this promotion.</li>
+                                    <li>SOLIS shall not be liable for rewards and /or benefits which accumulate inaccurately as a result of a technical malfunction, operator fault, misrepresentation or any reason beyond the control of Club SOLIS.</li>
+                                    <li>SOLISs reserves the right to modify or cancel promotion at its discretion without prior notice.  All points and membership card will be cancelled with the termination of membership.</li>
+                                    <li>SOLIS management shall be final.</li>
+                                    <li>The Chinese version of these Terms & Conditions shall prevail wherever there is any discrepancy between the English and Chinese version.</li>
+                                </ol>
+                            </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		<!-- Footer -->
+		<?php
+		include_once 'footer.html';
+		?>
+		<!-- /Footer -->
+	</footer>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.vm-carousel.js"></script>
+	<script src="js/index.js"></script>
+	<script>
+		$( window ).scroll( function ( event ) {
+
+
+			if ( $( this ).scrollTop() > 50 ) {
+
+				$( "#wrap" ).addClass( "fix-menu" );
+
+			} else {
+
+				$( "#wrap" ).removeClass( "fix-menu" );
+
+			}
+
+		} );
+
+		$( document ).ready( function () {
+
+			$( '.li_bt_home' ).removeClass( 'active' );
+			$( "#bs-navbar-collapse-1 li[class='dropdown']" ).addClass( 'active' );
+
+		} );
+
+		//    返回上一頁面
+		window.document.onkeydown = disableRefresh;
+
+		function disableRefresh( evt ) {
+			evt = ( evt ) ? evt : window.event
+			if ( evt.keyCode == 8 ) {
+				history.back();
+			}
+		}
+	</script>
+</body>
+
+</html>
